c7871287dbc3063e372336fd67cddbe85028c097
Sam Sherar <sbsherar@gmail.com>
2019-02-01 09:07:36 +0000

    Add informaton for the invitation on edit of group

--- wedding/blueprints/group.py
+++ wedding/blueprints/group.py
@@ -35,6 +35,19 @@ class SingleGroup(Resource):
         group = InvitationGroup.query.get_or_404(group_id)
         group.friendly_name = payload.get("name", group.friendly_name)
         group.group_code = payload.get("code", group.group_code)
+
+        inv_payload = payload.get("invitation", None)
+        if inv_payload is not None:
+            invitation = group.invitation
+            invitation.response = inv_payload.get("response", invitation.response)
+            invitation.invitation_type = inv_payload.get("type", invitation.invitation_type)
+            invitation.requirements = inv_payload.get("requirements", invitation.requirements)
+            invitation.plus_one = inv_payload.get("plus_one", invitation.plus_one)
+            if invitation.plus_one:
+                invitation.plus_one_name = inv_payload.get('plus_one_name', invitation.plus_one_name)
+            else:
+                invitation.plus_one_name = None
+            invitation.locked = inv_payload.get("locked", invitation.locked)
         db.session.commit()
         return {"status": "success", "message": "group updated"}, 200
 
